3b9d6013c354134d05392f5e95daaed1f2a49ec9
Ács Koppány <h986267@stud.u-szeged.hu>
2021-10-09 17:11:25 +0200

    Added tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,7 +1,21 @@
-describe('Example', function () {
+import {Greeter} from './Greeter'
 
-  it('should work', function () {
-    expect(true).toBe(true);
+describe('Handle a single input, multiple inputs, and no input.', function () {
+  let greeter:Greeter = new Greeter;
+  let result:String;
+  it('Handle no input', function () {
+    result = greeter.greets([]);
+    expect(result).toBe('Hello, my friend.');
+  });
+  
+  it('Handle single input', function () {
+    result = greeter.greets(['Bob']);
+    expect(result).toBe('Hello, Bob.');
+  });
+  
+  it('Handle multiple input', function () {
+    result = greeter.greets(['Alice', 'Bob', 'Jerry']);
+    expect(result).toBe('Hello, Alice, Bob, and Jerry.');
   });
 
 });
